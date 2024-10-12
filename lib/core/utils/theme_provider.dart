ca128d287896a0f06a229ca0fa68a0e058f2cd0a
moelsayed1 <mostafaembaby19@gmail.com>
2024-10-12 15:23:11 +0300

    add provider to my app

---
+++ lib/core/utils/theme_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  ThemeMode _themeMode = ThemeMode.system; // Default to system theme
+
+  ThemeMode get themeMode => _themeMode;
+
+  void toggleTheme() {
+    _themeMode =
+    _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    notifyListeners(); // Update widgets that listen to this provider
+  }
+}
