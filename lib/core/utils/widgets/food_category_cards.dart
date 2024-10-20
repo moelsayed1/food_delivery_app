8671accab1fc1cbed250928f4d3bd2ebee2aa303
moelsayed1 <mostafaembaby19@gmail.com>
2024-10-20 17:26:57 +0300

    create food_category_cards.dart

---
+++ lib/core/utils/widgets/food_category_cards.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class FoodCategoryCards extends StatelessWidget {
+  const FoodCategoryCards({super.key, required this.image, required this.label});
+
+  final String image;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 60,
+          width: 60,
+          decoration: BoxDecoration(
+            color: Colors.grey[600],
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.2),
+                spreadRadius: 2,
+                blurRadius: 5,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Center(
+            child: Image.asset(
+              image,
+              height: 30,
+              width: 30,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 12,
+          ),
+        ),
+      ],
+    );
+  }
+}
