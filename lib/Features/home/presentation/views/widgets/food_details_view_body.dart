35b34b75ca25ad088b035b7ab62108967ed8af73
moelsayed1 <mostafaembaby19@gmail.com>
2024-10-23 15:33:22 +0300

    make the floating button over the add-one-options

--- lib/Features/home/presentation/views/widgets/food_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/food_details_view_body.dart
@@ -12,7 +12,7 @@ class FoodDetailsViewBody extends StatefulWidget {
 class _FoodDetailsViewBodyState extends State<FoodDetailsViewBody> {
   int _quantity = 1;
 
-  Map<String, bool> _selectedAddons = {
+  final Map<String, bool> _selectedAddons = {
     'Pepper Julienned': false,
     'Broccoli': false,
     'Mashroom': false,
@@ -21,191 +21,183 @@ class _FoodDetailsViewBodyState extends State<FoodDetailsViewBody> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              AppBarIcons(
-                title: 'Food Details',
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              AspectRatio(
-                aspectRatio: 5 / 3.5,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(12),
-                  child: Image.asset(
-                    Assets.imagesPizza2,
-                    height: 250,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.only(bottom: 70), // Leave space for the floating button
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  const AppBarIcons(
+                    title: 'Food Details',
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  AspectRatio(
+                    aspectRatio: 5 / 3.5,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Image.asset(
+                        Assets.imagesPizza2,
+                        height: 250,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        'Special Pizza',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+                      const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Special Pizza',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Text(
+                            'With tomato sauce',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.orange,
+                          borderRadius: BorderRadius.circular(24),
                         ),
-                      ),
-                      SizedBox(
-                        height: 4,
-                      ),
-                      Text(
-                        'With tommato sauce',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.grey,
+                        child: Row(
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.remove, color: Colors.white),
+                              onPressed: () {
+                                setState(() {
+                                  if (_quantity > 1) {
+                                    _quantity--;
+                                  }
+                                });
+                              },
+                            ),
+                            Text(
+                              '$_quantity',
+                              style: const TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                              ),
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.add, color: Colors.white),
+                              onPressed: () {
+                                setState(() {
+                                  _quantity++;
+                                });
+                              },
+                            ),
+                          ],
                         ),
                       ),
                     ],
                   ),
-                  SizedBox(
-                    height: 8,
+                  const SizedBox(height: 16),
+                  Row(
+                    children: [
+                      const Icon(
+                        Icons.star,
+                        color: Colors.orange,
+                        size: 24,
+                      ),
+                      const SizedBox(width: 6),
+                      const Text(
+                        '4.3 Ratings',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      const Spacer(),
+                      Image.asset(
+                        Assets.imagesMessage,
+                        height: 24,
+                        color: Colors.grey,
+                      ),
+                      const SizedBox(width: 10),
+                      const Text(
+                        '960 Reviews',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
                   ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.orange, // Dark grey background
-                      borderRadius: BorderRadius.circular(24),
-                    ),
-                    child: Row(
-                      children: [
-                        IconButton(
-                          icon: const Icon(Icons.remove, color: Colors.white),
-                          onPressed: () {
-                            setState(() {
-                              if (_quantity > 1) {
-                                _quantity--;
-                              }
-                            });
-                          },
-                        ),
-                        Text(
-                          '$_quantity',
-                          style: const TextStyle(
-                            fontSize: 18,
-                            color: Colors.white,
-                          ),
-                        ),
-                        IconButton(
-                          icon: const Icon(Icons.add, color: Colors.white),
-                          onPressed: () {
-                            setState(() {
-                              _quantity++;
-                            });
-                          },
-                        ),
-                      ],
+                  const SizedBox(height: 24),
+                  const Text(
+                    'Detail & Ingredient',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
                   ),
-                ],
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              Row(
-                children: [
-                  Icon(
-                    Icons.star,
-                    color: Colors.orange,
-                    size: 24,
-                  ),
-                  SizedBox(
-                    width: 6,
-                  ),
+                  const SizedBox(height: 8),
                   Text(
-                    '4.3 Ratings',
+                    'Meat Lovers is filled with toppings of sliced beef sausage, minced beef, beef burger, and chicken sausage. In one bite, you can taste a variety of processed meats that are many and dense. Especially the minced meat which still has fiber in it.',
+                    maxLines: 6,
                     style: TextStyle(
                       fontSize: 16,
+                      color: Colors.grey[400],
                     ),
                   ),
-                  Spacer(),
-                  Image.asset(
-                    Assets.imagesMessage,
-                    height: 24,
-                    color: Colors.grey,
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    '960 Reviews',
+                  const SizedBox(height: 24),
+                  const Text(
+                    'Choice of Add On',
                     style: TextStyle(
-                      fontSize: 16,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
+                  const SizedBox(height: 16),
+                  _buildAddonOption(Assets.imagesPepper, 'Pepper Julienned', '\$2.30'),
+                  _buildAddonOption(Assets.imagesBroccoli, 'Broccoli', '4.70'),
+                  _buildAddonOption(Assets.imagesMushrooms, 'Mashroom', '\$2.50'),
+                  const SizedBox(height: 16),
                 ],
               ),
-              const SizedBox(height: 24),
-
-              // Description
-              const Text(
-                'Detail & Ingredient',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                'Meat Lovers is filled with toppings of sliced beef sausage, minced beef, beef burger, and chicken sausage. In one bite, you can taste a variety of processed meats that are many and dense. Especially the minced meat which still has fiber \nin it.',
-                maxLines: 6,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey[400], // Lighter grey for contrast
-                ),
-              ),
-              const SizedBox(height: 24),
-              const Text(
-                'Choice of Add On',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 16),
-              _buildAddonOption(Assets.imagesPepper, 'Pepper Julienned', '\$2.30'),
-              _buildAddonOption(Assets.imagesBroccoli, 'Broccoli', '4.70'),
-              _buildAddonOption(Assets.imagesMushrooms, 'Mashroom', '\$2.50'),
-              const SizedBox(height: 16),
-              Center(
-                child: SizedBox(
-                  height: 60,
-                  width: 200,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.orange,
-                      padding: const EdgeInsets.symmetric(vertical: 16),
-                      textStyle: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(32),
-                      ),
-                    ),
-                    child: const Text('Add Cart'),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-            ],
+            ),
           ),
-        ),
+          Positioned(
+            bottom: 20,
+            left: 100,
+            right: 100,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                backgroundColor: Colors.orange,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+              ),
+              onPressed: () {
+                // Handle Add to Cart action
+              },
+              child: const Text(
+                'Add Cart',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
