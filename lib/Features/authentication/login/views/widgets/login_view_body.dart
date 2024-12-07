67ca7eb65d2cc268b1f5b8c42b5d1d902e3e6cde
moelsayed1 <mostafaembaby19@gmail.com>
2024-12-07 20:28:59 +0200

    solve scroll view error

--- lib/Features/authentication/login/views/widgets/login_view_body.dart
+++ lib/Features/authentication/login/views/widgets/login_view_body.dart
@@ -30,6 +30,8 @@ class _LoginViewBodyState extends State<LoginViewBody> {
 
   bool obscureText = true;
 
+  bool isLoading = false;
+
   String? email;
 
   String? password;
@@ -41,190 +43,203 @@ class _LoginViewBodyState extends State<LoginViewBody> {
       child: Form(
         key: formKey,
         child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: ListView(
             children: [
-              const AppBarIcons(),
-              const SizedBox(height: 100),
-              const Text(
-                'LOGIN',
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.w700),
-              ),
-              const SizedBox(height: 30),
-              const SizedBox(height: 20),
-              CustomTextField(
-                onChanged: (value) {
-                  email = value;
-                  log('$email');
-                },
-                hintText: 'Enter Your Email',
-                labelText: 'Email',
-                keyboardType: TextInputType.emailAddress,
-                controller: emailController,
-              ),
-              const SizedBox(height: 20),
-              TextFormField(
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter password';
-                  }
-                  return null;
-                },
-                onChanged: (value) {
-                  password = value;
-                  log('$password');
-                },
-                controller: passwordController,
-                obscureText: obscureText,
-                decoration: InputDecoration(
-                  border: AppTheme.darkTheme.inputDecorationTheme.border,
-                  enabledBorder: AppTheme.darkTheme.inputDecorationTheme.enabledBorder,
-                  focusedBorder: AppTheme.darkTheme.inputDecorationTheme.focusedBorder,
-                  labelText: 'Password',
-                  suffixIcon: IconButton(
-                    icon: Icon(obscureText ? Icons.visibility : Icons.visibility_off),
-                    onPressed: () {
-                      obscureText = !obscureText;
-
-                    },
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const AppBarIcons(),
+                  const SizedBox(height: 100),
+                  const Text(
+                    'LOGIN',
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.w700),
                   ),
-                ),
-              ),
-              const SizedBox(height: 35),
-              Center(
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, AppRouter.forgetRoute);
-                  },
-                  child: Text(
-                    'Forget Password?',
-                    style: TextStyle(fontSize: 16, color: AppTheme.darkTheme.primaryColor),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 35),
-              Center(
-                child: SizedBox(
-                  height: 55,
-                  width: 200,
-                  child: CustomElevatedButton(
-                    onPressed: () async {
-                      if (formKey.currentState!.validate()) {
-                        widget.onLoadingChanged(true);
-                        try {
-                          UserCredential user = await signInUser();
-                          Navigator.pushNamed(context, AppRouter.homeRoute);
-                          log(user.user!.uid);
-                        } on FirebaseAuthException catch (e) {
-                          if (e.code == 'wrong-password') {
-                            buildShowSnackBar(context, 'Wrong password provided for that user.');
-                          } else if (e.code == 'user-not-found') {
-                            buildShowSnackBar(context, 'No user found for that email.');
-                          }
-                        } catch (e) {
-                          log(e.toString());
-                        } finally {
-                          widget.onLoadingChanged(false);
-                        }
+                  const SizedBox(height: 30),
+                  const SizedBox(height: 20),
+                  CustomTextField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter this field';
                       }
+                      return null;
                     },
-
-                    child: const Text('Login', style: TextStyle(fontSize: 18)),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 35),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('Don\'t have an account?', style: TextStyle(fontSize: 16)),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, AppRouter.signupRoute);
+                    onChanged: (value) {
+                      email = value;
+                      log('$email');
                     },
-                    child: const Text(
-                      'Sign Up',
-                      style: TextStyle(fontSize: 16, color: Color(0xffFF5722)),
-                    ),
+                    hintText: 'Enter Your Email',
+                    labelText: 'Email',
+                    keyboardType: TextInputType.emailAddress,
+                    controller: emailController,
                   ),
-                ],
-              ),
-              const SizedBox(height: 35),
-              Row(
-                children: [
-                  Expanded(child: Divider(thickness: 1, color: Colors.white.withOpacity(0.5))),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16),
-                    child: Text('Sign Up with', style: TextStyle(fontSize: 16)),
-                  ),
-                  Expanded(child: Divider(thickness: 1, color: Colors.white.withOpacity(0.5))),
-                ],
-              ),
-
-              const SizedBox(height: 30),
-              Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      height: 50,
-                      width: 160,
-                      child: ElevatedButton.icon(
-                        onPressed: () async {
-                          widget.onLoadingChanged(true);
-                          try {
-                            UserCredential? userCredential =
-                            await signInWithFacebook();
-
-                            if (userCredential != null) {
-                              // Navigate to the next screen
-                              Navigator.pushNamed(
-                                  context, AppRouter.homeRoute);
-                            } else {
-                              buildShowSnackBar(context,
-                                  'Facebook sign-in failed or canceled.');
-                            }
-                          } catch (e) {
-                            log("Facebook Sign-In Error: $e");
-                            buildShowSnackBar(
-                                context, 'Facebook sign-in error.');
-                          } finally {
-                            widget.onLoadingChanged(false);
-                          }
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter password';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) {
+                      password = value;
+                      log('$password');
+                    },
+                    controller: passwordController,
+                    obscureText: obscureText,
+                    decoration: InputDecoration(
+                      border: AppTheme.darkTheme.inputDecorationTheme.border,
+                      enabledBorder: AppTheme.darkTheme.inputDecorationTheme.enabledBorder,
+                      focusedBorder: AppTheme.darkTheme.inputDecorationTheme.focusedBorder,
+                      labelText: 'Password',
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          obscureText ? Icons.visibility_off : Icons.visibility,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            obscureText = !obscureText;
+                          });
                         },
-                        style: ElevatedButton.styleFrom(backgroundColor: const Color(0xff1877F2)),
-                        label: const Text('Facebook', style: TextStyle(fontSize: 16)),
-                        icon: Image.asset(Assets.imagesLogoFacebook, height: 30),
                       ),
                     ),
-                    SizedBox(
-                      height: 50,
-                      width: 160,
-                      child: ElevatedButton.icon(
+                  ),
+                  const SizedBox(height: 35),
+                  Center(
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, AppRouter.forgetRoute);
+                      },
+                      child: Text(
+                        'Forget Password?',
+                        style: TextStyle(fontSize: 16, color: AppTheme.darkTheme.primaryColor),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 35),
+                  Center(
+                    child: SizedBox(
+                      height: 55,
+                      width: 200,
+                      child: CustomElevatedButton(
                         onPressed: () async {
-                          widget.onLoadingChanged(true);
-                          try {
-                            UserCredential? userCredential = await signInWithGoogle();
-                            if (userCredential != null) {
-                              log("Google sign-in successful!");
+                          if (formKey.currentState!.validate()) {
+                            widget.onLoadingChanged(true);
+                            try {
+                              UserCredential user = await signInUser();
                               Navigator.pushNamed(context, AppRouter.homeRoute);
-                            } else {
-                              buildShowSnackBar(context, 'Google sign-in failed or canceled.');
+                              log(user.user!.uid);
+                            } on FirebaseAuthException catch (e) {
+                              if (e.code == 'wrong-password') {
+                                buildShowSnackBar(context, 'Wrong password provided for that user.');
+                              } else if (e.code == 'user-not-found') {
+                                buildShowSnackBar(context, 'No user found for that email.');
+                              }
+                            } catch (e) {
+                              log(e.toString());
+                            } finally {
+                              widget.onLoadingChanged(false);
                             }
-                          } catch (e) {
-                            log("Google Sign-In Error: $e");
-                            buildShowSnackBar(context, 'An error occurred during Google sign-in.');
-                          } finally {
-                            widget.onLoadingChanged(false);
                           }
                         },
-                        style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
-                        label: Text('Google', style: TextStyle(fontSize: 16, color: Colors.black.withOpacity(0.5))),
-                        icon: Image.asset(Assets.imagesLogoGoogle, height: 25),
+
+                        child: const Text('Login', style: TextStyle(fontSize: 18)),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  const SizedBox(height: 35),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('Don\'t have an account?', style: TextStyle(fontSize: 16)),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, AppRouter.signupRoute);
+                        },
+                        child: const Text(
+                          'Sign Up',
+                          style: TextStyle(fontSize: 16, color: Color(0xffFF5722)),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 35),
+                  Row(
+                    children: [
+                      Expanded(child: Divider(thickness: 1, color: Colors.white.withOpacity(0.5))),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16),
+                        child: Text('Sign Up with', style: TextStyle(fontSize: 16)),
+                      ),
+                      Expanded(child: Divider(thickness: 1, color: Colors.white.withOpacity(0.5))),
+                    ],
+                  ),
+
+                  const SizedBox(height: 30),
+                  Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SizedBox(
+                          height: 50,
+                          width: 160,
+                          child: ElevatedButton.icon(
+                            onPressed: () async {
+                              widget.onLoadingChanged(true);
+                              try {
+                                UserCredential? userCredential =
+                                await signInWithFacebook();
+
+                                if (userCredential != null) {
+                                  // Navigate to the next screen
+                                  Navigator.pushNamed(
+                                      context, AppRouter.homeRoute);
+                                } else {
+                                  buildShowSnackBar(context,
+                                      'Facebook sign-in failed or canceled.');
+                                }
+                              } catch (e) {
+                                log("Facebook Sign-In Error: $e");
+                                buildShowSnackBar(
+                                    context, 'Facebook sign-in error.');
+                              } finally {
+                                widget.onLoadingChanged(false);
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(backgroundColor: const Color(0xff1877F2)),
+                            label: const Text('Facebook', style: TextStyle(fontSize: 16)),
+                            icon: Image.asset(Assets.imagesLogoFacebook, height: 30),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 50,
+                          width: 160,
+                          child: ElevatedButton.icon(
+                            onPressed: () async {
+                              widget.onLoadingChanged(true);
+                              try {
+                                UserCredential? userCredential = await signInWithGoogle();
+                                if (userCredential != null) {
+                                  log("Google sign-in successful!");
+                                  Navigator.pushNamed(context, AppRouter.homeRoute);
+                                } else {
+                                  buildShowSnackBar(context, 'Google sign-in failed or canceled.');
+                                }
+                              } catch (e) {
+                                log("Google Sign-In Error: $e");
+                                buildShowSnackBar(context, 'An error occurred during Google sign-in.');
+                              } finally {
+                                widget.onLoadingChanged(false);
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+                            label: Text('Google', style: TextStyle(fontSize: 16, color: Colors.black.withOpacity(0.5))),
+                            icon: Image.asset(Assets.imagesLogoGoogle, height: 25),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
